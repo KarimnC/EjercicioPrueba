8f6bb80a0b803286782b904c3bf61a830be3c56b
KarimnC <46898251+KarimnC@users.noreply.github.com>
2019-01-21 09:40:24 -0600

    Karimn Daniel Hernández Castorena, Grupo 02.

---
+++ VelPromedio.py
@@ -0,0 +1,9 @@
+# Autor: Karimn Daniel
+# Calcular la vel. promedio en un viaje.
+
+tiempo = int(input("A ver prro, escribe el tiempo de viaje en horas enteras: "))
+distancia = int(input("Shavo, ahora escribe la distancia del viaje en kilómetros enteros: "))
+
+velocidad = distancia/tiempo
+
+print("A nu ma, la velocidad es: ", velocidad, "km/h")
